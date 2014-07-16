7156b8906b2ddd8a7b8741ccc47d097d7427017e
Cheng Yichao <onesuperclark@gmail.com>
2014-07-16 15:50:42 +0800

    Add a case to test mutex_lock

---
+++ test/test_mutex.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
+
+
+void do_something(void) {
+  int i, a;
+  for (i = 0; i < 1000; i++) a++;
+}
+
+
+void *worker(void *data) {
+  pthread_mutex_lock(&g_lock);
+
+  do_something();
+
+  pthread_mutex_unlock(&g_lock);
+
+  return NULL;
+}
+
+
+int main(int argc, char **argv) {
+  const int nThreads = 8;
+  pthread_t workers[nThreads];
+  int i;
+
+  for (i = 0; i < nThreads; i++)
+    pthread_create(&workers[i], NULL, worker, NULL);
+
+  for (i = 0; i < nThreads; i++)
+    pthread_join(workers[i], NULL);
+
+  return 0;
+}
